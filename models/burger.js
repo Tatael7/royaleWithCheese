799f9375dd4c2647c8543a43ffa61f8ea7cebaa2
T4T4el7 <ochoaarmando13@gmail.com>
2019-03-16 23:50:33 -0700

    arranged orm calls

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,21 @@
 const orm = require("../db/config/orm.js");
 
-//module.exports = burger;
+var burger = {
+    selectAll: function(whatToSelect, tableInput, cb) {
+        orm.selectAll(whatToSelect, tableInput, function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(tableInput, burger_name, devoured, cb) {
+        orm.insertOne(tableInput, burger_name, devoured, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(whatToSelect, tableInput, changesDesired, cb) {
+        orm.updateOne(whatToSelect, tableInput, changesDesired, function(res) {
+            cb(res);
+        });
+    }
+    
+};
+module.exports = burger;
